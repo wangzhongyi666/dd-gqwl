c35972c3aaaee7c34921757fcabf8933825b8b85
zhengshizhao0706 <zhengshizhao0706@163.com>
2018-08-26 23:20:21 +0800

    修改按钮权限

--- src/main/java/com/dongdao/gqwl/filter/UrlFilter.java
+++ src/main/java/com/dongdao/gqwl/filter/UrlFilter.java
@@ -32,100 +32,71 @@ public class UrlFilter implements Filter {
     // 自动注入，不需要生成set方法了，required=false表示没有实现类，也不会报错。
     public SysMenuBtnService sysMenuBtnService;
 
-    protected static List<Pattern> patterns = new ArrayList<Pattern>();
+
+    //标示符：表示当前用户未登录(可根据自己项目需要改为json样式)
+    String NO_LOGIN = "您还未登录";
+
+    //不需要登录就可以访问的路径(比如:注册登录等)
+    String[] includeUrls = new String[]{"/guangqi/tologin.do","/guangqi/login.shtml","/"};
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String uri = request.getRequestURI();
+
+        System.out.println("filter url:"+uri);
+        //是否需要过滤
+        boolean needFilter = isNeedFilter(uri);
+
+
+        if (!needFilter) { //不需要过滤直接传给下一个过滤器
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else { //需要过滤器
+            // session中包含user对象,则是登录状态
+            if(SessionUtils.getUser(request) != null){
+                // System.out.println("user:"+session.getAttribute("user"));
+                filterChain.doFilter(request, response);
+            }else{
+                String requestType = request.getHeader("X-Requested-With");
+                //判断是否是ajax请求
+                if(requestType!=null && "XMLHttpRequest".equals(requestType)){
+                    response.getWriter().write(this.NO_LOGIN);
+                }else{
+                    //重定向到登录页(需要在static文件夹下建立此html文件)
+                    response.sendRedirect(request.getContextPath()+"/guangqi/login.shtml");
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * @Author: xxxxx
+     * @Description: 是否需要过滤
+     * @Date: 2018-03-12 13:20:54
+     * @param uri
+     */
+    public boolean isNeedFilter(String uri) {
+
+        for (String includeUrl : includeUrls) {
+            if(includeUrl.equals(uri)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
-                   FilterChain filterChain) throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        //打印请求Url
-        //验证用户是否登录
-        //如果用户登录 验证url是否正确
-        //验证url  .do  , .shtml
-
-
-       /* List<SysMenu> rootMenus = null;
-        System.out.println("this is MyFilter,url :" + request.getRequestURI());
-        rootMenus = sysMenuService.getAllMenu();// 查询所有根节点
-        if(!request.getRequestURI().equals("/")){
-            if(rootMenus!=null&&rootMenus.size()>0){
-                for (SysMenu menu : rootMenus){
-                    if(menu!=null&&menu.getUrl()!=null&&menu.getUrl().equals(request.getRequestURI())){
-                        SysUser user = SessionUtils.getUser(request);
-                        if(user==null){
-                            response.sendRedirect(URLUtils.get("msUrl")+"/guangqi/login.shtml");
-                            return;
-                        }
-                    }else{
-                        response.sendRedirect(URLUtils.get("msUrl")+"/guangqi/login.shtml");
-                        return;
-                    }
-                }
-            }
-        }*/
-
-        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-       // logger.info("aaaaaaaaaa");
-        String url = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
-        if (url.startsWith("/") && url.length() > 1) {
-            url = url.substring(1);
-        }
-
-        if (isInclude(url)){
-            filterChain.doFilter(httpRequest, httpResponse);
-            return;
-        } else {
-            SysUser user = SessionUtils.getUser(request);
-            if (user != null){
-                // session存在
-                filterChain.doFilter(httpRequest, httpResponse);
-                return;
-            } else {
-                // session不存在 准备跳转失败
-        /* RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-          dispatcher.forward(request, response);*/
-                System.out.println("this is MyFilter,url :" + request.getRequestURI());
-                List<SysMenu> rootMenus = sysMenuService.getAllMenu();// 查询所有根节点
-                for (SysMenu menu : rootMenus){
-                    if(menu!=null&&menu.getUrl()!=null&&menu.getUrl().equals(request.getRequestURI())){
-                        response.sendRedirect(URLUtils.get("msUrl")+"/guangqi/login.shtml");
-                        return;
-                    }
-                }
-                filterChain.doFilter(httpRequest, httpResponse);
-                return;
-            }
-        }
-        //System.out.println("this is MyFilter,url :" + request.getRequestURI());
-        //filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
     public void destroy() {
 
     }
-
-    /**
-     * 是否需要过滤
-     * @param url
-     * @return
-     */
-    private boolean isInclude(String url) {
-        Pattern p = Pattern.compile("\\*.shtml|.do");
-        patterns.add(p);
-        for (Pattern pattern : patterns) {
-            Matcher matcher = pattern.matcher(url);
-            return matcher.find();
-        }
-        return false;
-    }
-
 }
 
